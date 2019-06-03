dd8191accb850b9aa7cbe11420e41ae7da99235b
noemie <e70838@gmail.com>
2019-06-03 11:27:27 +0200

    Initial commit

---
+++ src/main/java/com/galaxy/mental/test_socket/MainActivity.java
@@ -0,0 +1,69 @@
+package com.galaxy.mental.test_socket;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        new Thread(new ClientThread()).start();
+    }
+    private Socket socket;
+
+    private static final int SERVERPORT = 9000;
+    private static final String SERVER_IP = "172.22.18.3";
+
+
+    public void onClick(View view) {
+        try {
+            String str = "toto";
+            PrintWriter out = new PrintWriter(new BufferedWriter(
+                    new OutputStreamWriter(socket.getOutputStream())),
+                    true);
+            out.println(str);
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    class ClientThread implements Runnable {
+
+        @Override
+        public void run() {
+
+            try {
+                InetAddress serverAddr = InetAddress.getByName(SERVER_IP);
+
+                socket = new Socket(serverAddr, SERVERPORT);
+
+            } catch (UnknownHostException e1) {
+                e1.printStackTrace();
+            } catch (IOException e1) {
+                e1.printStackTrace();
+            }
+
+        }
+
+    }
+
+}
